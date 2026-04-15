440fc6788859ebcbc8d125a08a4937603b82ba63
kimyubi <ieieie0419@gmail.com>
2026-04-15 13:56:03 +0900

    feat : add Order entity

--- src/main/java/com/sparta/miniorder/order/entity/Order.java
+++ src/main/java/com/sparta/miniorder/order/entity/Order.java
@@ -23,8 +23,6 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // 주문은 상품 1개만 참조 (요구사항)
-    // 상품 이름을 복사해서 저장하지 않고, 연관관계로 연결 → 상품 이름 변경 시 주문 조회에도 반영됨
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
